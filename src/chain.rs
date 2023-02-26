d76d1d255c98181a478babf211f9753fc41b8130
Thibault <contact@thibaultmeunier.com>
2023-02-26 12:07:20 +0100

    Add documentation for chain package

--- src/chain.rs
+++ src/chain.rs
@@ -5,55 +5,76 @@ use serde::{Deserialize, Serialize};
 use crate::beacon::RandomnessBeacon;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
+/// Additional information about the chain.
 pub struct ChainMetadata {
     #[serde(rename(serialize = "beaconID", deserialize = "beaconID"))]
-    beacon_id: String, // the ID of the beacon chain this `ChainInfo` corresponds to
+    beacon_id: String,
+}
+
+impl ChainMetadata {
+    pub fn new(beacon_id: String) -> Self {
+        Self { beacon_id }
+    }
+
+    /// The ID of the beacon chain this `ChainInfo` corresponds to.
+    pub fn beacon_id(&self) -> String {
+        self.beacon_id.clone()
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct ChainInfo {
-    public_key: String, // hex encoded BLS12-381 public key
-    period: u64,        // how often the network emits randomness (in seconds)
-    genesis_time: u64,  // the time of the round 0 of the network (in epoch seconds)
-    hash: String,       // the hash identifying this specific chain of beacons
+    public_key: String,
+    period: u64,
+    genesis_time: u64,
+    hash: String,
     #[serde(rename(serialize = "groupHash", deserialize = "groupHash"))]
-    group_hash: String, // a hash of the group file containing details of all the nodes participating in the network
+    group_hash: String,
     #[serde(rename(serialize = "schemeID", deserialize = "schemeID"))]
-    scheme_id: String, // the version/format of cryptography
+    scheme_id: String,
     metadata: ChainMetadata,
 }
 
 impl ChainInfo {
+    /// Hex encoded BLS12-381 public key.
     pub fn public_key(&self) -> String {
         self.public_key.clone()
     }
 
+    /// How often the network emits randomness (in seconds).
     pub fn period(&self) -> u64 {
         self.period
     }
 
+    /// Time of the round 0 of the network (in epoch seconds).
     pub fn genesis_time(&self) -> u64 {
         self.genesis_time
     }
 
+    /// Hash identifying this specific chain of beacons.
     pub fn hash(&self) -> String {
         self.hash.clone()
     }
 
+    /// A hash of the group file containing details of all the nodes participating in the network.
     pub fn group_hash(&self) -> String {
         self.group_hash.clone()
     }
 
+    /// The version/format of cryptography.
     pub fn scheme_id(&self) -> String {
         self.scheme_id.clone()
     }
 
+    /// Additional information about the chain.
     pub fn metadata(&self) -> ChainMetadata {
         self.metadata.clone()
     }
 }
 
 #[derive(Debug, Clone)]
+/// HTTP drand chain, identified by a base URL
+/// e.g https://drand.cloudflare.com
 pub struct Chain {
     base_url: String,
 }
@@ -78,6 +99,8 @@ impl Chain {
 }
 
 #[derive(Debug, Clone)]
+/// Retrieval and validation options when interacting with a chain.
+/// This controls beacons validation, chain validation, and cache on retrieval.
 pub struct ChainOptions {
     is_beacon_verification: bool,
     is_cache: bool,
@@ -121,6 +144,7 @@ impl Default for ChainOptions {
 }
 
 #[derive(Debug, Clone)]
+/// Parameters that can be used to validate a chain is the expected one.
 pub struct ChainVerification {
     hash: Option<String>,
     public_key: Option<String>,
@@ -151,9 +175,16 @@ impl Default for ChainVerification {
 }
 
 #[async_trait]
+/// Drand client, that can retrieve and validate information from a given chain.
 pub trait ChainClient {
+    /// Options that are used to validate chain result.
     fn options(&self) -> ChainOptions;
+    /// Retrieve latest beacon.
+    /// This is retrieved and validated based on the client options.
     async fn latest(&self) -> Result<RandomnessBeacon>;
+    /// Retrieve specific round beacon.
+    /// This is retrieved and validated based on the client options.
     async fn get(&self, round_number: u64) -> Result<RandomnessBeacon>;
+    /// Chain the client is associated to.
     fn chain(&self) -> Chain;
 }
